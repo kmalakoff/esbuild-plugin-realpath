da886f649a7b040e211388ee574dfff4e8f9d028
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-11 20:13:09 -0700

    Update Biome configuration and dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { promises as fs } from 'fs';
-import { promisify } from 'util';
 import browserResolve_ from 'browser-resolve';
 import type { Plugin, PluginBuild } from 'esbuild';
+import { promises as fs } from 'fs';
 import resolve_ from 'resolve';
 import resolveOnce from 'resolve-once';
+import { promisify } from 'util';
 
 const nodeResolve = promisify(resolve_);
 const browserResolve = promisify(browserResolve_);
@@ -18,7 +18,7 @@ export default (optionsIn = {}): Plugin => {
         ...optionsIn,
       };
       const resolve = build.initialOptions.platform === 'node' ? nodeResolve : browserResolve;
-      build.onResolve({ filter: /^[^.\/]|^\.[^.\/]|^\.\.[^\/]/ }, async (args) => {
+      build.onResolve({ filter: /^[^./]|^\.[^./]|^\.\.[^/]/ }, async (args) => {
         if (!cache[args.path]) {
           cache[args.path] = resolveOnce(async () => {
             try {
