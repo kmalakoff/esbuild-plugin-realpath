kmalakoff/esbuild-plugin-progress

Upgrade modules

test/exports/import.test.mjs
@@ -1,0 +1,8 @@
+import assert from 'assert';
+import progress from 'esbuild-plugin-progress';
+
+describe('exports .mjs', () => {
+  it('default', () => {
+    assert.equal(typeof progress, 'function');
+  });
+});

test/exports/import.test.ts
@@ -1,0 +1,8 @@
+import assert from 'assert';
+import progress from 'esbuild-plugin-progress';
+
+describe('exports .ts', () => {
+  it('default', () => {
+    assert.equal(typeof progress, 'function');
+  });
+});

test/exports/require.test.cjs
@@ -1,0 +1,8 @@
+const assert = require('assert');
+const progress = require('esbuild-plugin-progress');
+
+describe('exports .cjs', () => {
+  it('default', () => {
+    assert.equal(typeof progress, 'function');
+  });
+});

kmalakoff/esbuild-plugin-realpath

test/exports/import.test.mjs
@@ -1,0 +1,8 @@
+import assert from 'assert';
+import realpath from 'esbuild-plugin-realpath';
+
+describe('exports .mjs', () => {
+  it('default', () => {
+    assert.equal(typeof realpath, 'function');
+  });
+});

test/exports/import.test.ts
@@ -1,0 +1,8 @@
+import assert from 'assert';
+import realpath from 'esbuild-plugin-realpath';
+
+describe('exports .ts', () => {
+  it('default', () => {
+    assert.equal(typeof realpath, 'function');
+  });
+});

test/exports/require.test.cjs
@@ -1,0 +1,8 @@
+const assert = require('assert');
+const realpath = require('esbuild-plugin-realpath');
+
+describe('exports .cjs', () => {
+  it('default', () => {
+    assert.equal(typeof realpath, 'function');
+  });
+});